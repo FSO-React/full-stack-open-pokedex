4aacb930655359de1d18a789ee3d0d0a154ce93b
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-06-23 23:38:09 -0300

    exercise 11.8

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import 'dotenv/config'
-import express from 'express'
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
 const app = express()
 
 // get the port from env variable
